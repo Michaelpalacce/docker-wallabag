af5960baa5812d7501c9d9ebd4fde8d848de498f
Stefan <stefantigro@gmail.com>
2022-01-17 22:26:48 +0200

    [main] Initial

---
+++ BUILD.sh
@@ -0,0 +1,11 @@
+# GET LATEST VERSION FROM: https://github.com/wallabag/wallabag
+VERSION=$(curl -s -XGET https://api.github.com/repos/wallabag/wallabag/tags | grep name -m 1 | awk '{print $2}' | cut -d'"' -f2)
+
+docker buildx build --platform linux/amd64,linux/arm64 \
+-t stefangenov/wallabag:latest \
+-t stefangenov/wallabag:"${VERSION}" \
+-f Dockerfile \
+--build-arg TAG_VERSION="${VERSION}" \
+--cpu-quota="400000" \
+--memory=8g \
+--push .
--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-# docker-wallabag
+Docker image with ARM and AMD builds to install wallabag.
+This automatically fetches the latest tag from github.
+
+Dockerfile taken from: https://github.com/wallabag/docker.
+root folder taken from: https://github.com/wallabag/docker
+
+Docker hub: https://hub.docker.com/repository/docker/stefangenov/wallabag
